5c33756338a429645a779bad29e537e710b25ac3
Eduardo <eduardo1618@gmail.com>
2018-03-09 22:53:48 -0600

    Initial Commit

---
+++ Pods/Target Support Files/Pods-cv-gigigo/Pods-cv-gigigo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cv_gigigo : NSObject
+@end
+@implementation PodsDummy_Pods_cv_gigigo
+@end
---
+++ cv-gigigo/GGResumeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  cv-gigigo
+//
+//  Created by Eduardo Miñon on 09/03/18.
+//  Copyright © 2018 Eduardo Miñon. All rights reserved.
+//
+
+import UIKit
+
+class GGResumeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
